2f3275fa1f001093f00852a401674df531d808b7
arybyt <bayatiarya@gmail.com>
2021-05-18 10:46:39 -0500

    workingIcon

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ async function getCity(city) {
  
   try {
     let response = await axios.get(locationWeather)
-    console.log(response.data)
+    console.log(response.data.current.is_day)
     const data = response.data
     cityName(data)
     return data
@@ -18,19 +18,31 @@ async function getCity(city) {
 }
 function cityName(data) {
   // console.log(data.location.name)
+
+  let cityIcon = `${data.current.condition.icon}`
+  let workingIcon = cityIcon.replace("//", "https://")
+console.log(workingIcon)
+
+
   let cityNames = `
   <div class="locationCard"
   <h3 id="names">${data.location.name}</h3>
-  <img id="ax" src="https://darkhunts.com/wp-content/uploads/2020/05/fix-internet-connection.png">
-  <p>${data.current.temp_f}°F</p>
-  <h4>${data.current.cloud}</h4>
-  <h5>${data.current.condition.text}</h5>
+  <img id="ax" src="https://b.kisscc0.com/20180705/yq/kisscc0-cloud-sunlight-drawing-sky-storm-clouds-with-hidden-sun-5b3e5f18ecd5c0.1237714015308142329701.png">
+  <img id="iconImg" src="${workingIcon}"
+  <p>Temperature is: ${data.current.temp_f}°F</p>
+  <h4>Feels like: ${data.current.feelslike_f}°F</h4>
+  <h5>And it's: ${data.current.condition.text}</h5>
   </div>
   `
-  console.log(cityNames)
+  // console.log(cityNames)
   locationName.insertAdjacentHTML("beforeend", cityNames)
   return cityNames
 }
+
+// let time = null
+// if (`${data.current.is_day}`=== 0) {
+  
+// }else
  
 const form = document.querySelector(".location")
 // console.log(form)
