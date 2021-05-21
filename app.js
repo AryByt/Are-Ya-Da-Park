459d1e58bb1c9388cde32d9f85487c2620495df6
arybyt <bayatiarya@gmail.com>
2021-05-21 01:53:22 -0500

    2am work

--- app.js
+++ app.js
@@ -1,75 +1,65 @@
 const key = "8dd21966983c4da092a154427211705"
 const base = "http://api.weatherapi.com/v1/current.json"
-const astroBase = "http://api.weatherapi.com/v1/astronomy.json"
 const locationName = document.querySelector("#location-card")
 
-  async function getCity(city) {
-    const locationWeather = `${base}?key=${key}&q=${city}`
-    try {
-      let response = await axios.get(locationWeather)
-        console.log(response.data)
-           const data = response.data
-            cityName(data)
-      return data
-       } catch (error) {
-          console.error(error)
-       }
+async function getCity(city) {
+  const locationWeather = `${base}?key=${key}&q=${city}`
+  try {
+    let response = await axios.get(locationWeather)
+    const data = response.data
+    cityName(data)
+    return data
+  } catch (error) {
+    console.error(error)
   }
+}
     
 
 
-function cityName(data) {
+  function cityName(data) {
   let cityIcon = `${data.current.condition.icon}`
   let workingIcon = cityIcon.replace("//", "https://")
-  console.log(workingIcon)
-  
+  console.log(data)
 
   let dayOrNightImg=""
-  if (data.current.is_day == 0) {
-    dayOrNightImg="./nightTimeImg.png"
+   if (data.current.is_day == 0) {
+    dayOrNightImg="./600x600-night.jpeg"
   }
   else {
-    dayOrNightImg="./dayTimeImg.png"
+    dayOrNightImg="./day-picture.jpeg"
   }
 
 
   let cityNames = `
-  <div class="locationCard">
+  <div class="location-card">
     <section class="image-container">
-      <img id="dayOrNightPic" src="${dayOrNightImg}">
-      <img id="iconImg" src="${workingIcon}">
+       <img id="day-or-night-img" src="${dayOrNightImg}">
+        <img id="icon-img" src="${workingIcon}">
     </section>
-    <h1 id="daName">${data.location.name}</h1>
+    <h1 id="city-name">${data.location.name}</h1>
     <h2>In normal It's: ${data.current.temp_c}°C</h2>
     <h3>In American It's: ${data.current.temp_f}°F</h3>
     <h5>It's: ${data.current.condition.text}</h5>
     <h1>Feels like:${data.current.feelslike_f}°F</h1>
-    <h2>Humidity is:${data.current.humidity}</h2>
-    <h3>Wind mph:${data.current.wind_mph}</h3>
+    <h2>Humidity is:${data.current.humidity}%</h2>
+    <h3>Wind : ${data.current.wind_mph} : mph</h3>
   </div>
-  <footer>${data.location.name} ${data.location.localtime}</footer>
+  <footer>State: ${data.location.region}, City: ${data.location.name},${data.location.localtime}</footer>
   `
   locationName.insertAdjacentHTML("beforeend", cityNames)
-  return cityNames
-
-
- }
-
- 
+    return cityNames
+}
 const form = document.querySelector(".location")
 form.addEventListener("submit", (e) => {
   e.preventDefault()
   const input = document.querySelector("#city-search").value
   removeCity(locationName)
   getCity(input)
-  document.querySelector("#city-search").value= " "
+  document.querySelector("#city-search").value = ""
 })
-
-
 function removeCity(removingCity) {
   while (removingCity.lastChild) {
     removingCity.removeChild(locationName.lastChild)
   }
 }
-
 
